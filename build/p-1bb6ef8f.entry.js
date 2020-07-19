23164037de6a824ad93f805db576a63b10658f3b
SeanWong24 <seanwong24@live.com>
2020-07-19 22:24:10 +0000

    Deploying to gh-pages from  @ 60ad8407e4f6006f6acbd15d16ac8c0cebed5df6 🚀

---
+++ build/p-1bb6ef8f.entry.js
@@ -0,0 +1,1 @@
+import{r as o,h as n,H as r}from"./p-cd08f63e.js";const l=class{constructor(n){o(this,n)}render(){return n(r,null,n("ion-header",null,n("ion-toolbar",{color:"primary"},n("ion-title",null,"Game"))),n("ion-content",{class:"ion-padding"},n("ion-text",null,"Game works!")))}};l.style=":host{display:block}";export{l as app_game}
